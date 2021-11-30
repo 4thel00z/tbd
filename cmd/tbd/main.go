39baee9a02360ab897b7f7a87bbb75a6c513f0c8
4thel00z <4thel00z@gmail.com>
2021-12-01 00:14:32 +0100

    [master] Add flags tbd main.go
    Signed-off-by: 4thel00z <4thel00z@gmail.com>

--- cmd/tbd/main.go
+++ cmd/tbd/main.go
@@ -8,8 +8,11 @@ import (
 )
 
 var (
-	path  = flag.String("path", "", "Path to the file to be compiled and built")
-	debug = flag.Bool("debug", false, "Do you want to read debug output")
+	path   = flag.String("path", "", "Path to the file to be compiled and built")
+	debug  = flag.Bool("debug", false, "Do you want to read debug output")
+	owner  = flag.String("owner", "4thel00z", "Owner of the repository (for uploading the artefacts)")
+	repo   = flag.String("repo", "", "Name of the repository (for uploading the artefacts)")
+	apiKey = flag.String("api-key", "", "API key for github - it needs to have private repo perms")
 )
 
 func main() {
@@ -18,7 +21,23 @@ func main() {
 		flag.Usage()
 		os.Exit(1)
 	}
-	tbd, err := libtbd.DefaultTBD(*debug)
+
+	if *owner == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if *repo == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if *apiKey == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	tbd, err := libtbd.DefaultTBD(*owner, *repo, *apiKey, *debug)
 	libtbd.Must(err)
 	res, err := tbd.BuildImage(*path)
 	libtbd.Must(err)
